f41261cec3ad96cc3904e6b71c1d18c3a96de0c9
DcViet <tranducviet.eng@gmail.com>
2023-06-22 01:23:49 +0700

    update

---
+++ TimeToJavascript/javascript/learn-to-code.js
@@ -0,0 +1,16 @@
+// var name = "Jack";
+// var age = 23;
+// var shootingScore = 45.6;
+// var message = "Hi, my name is " + name + " and I am " + age + " year old! ";
+
+// var firstName = "John";
+// var lastName = "JacobJingleHeimerSmith";
+// var dateOfBirth = "10-09-82";
+// var age = 23;
+// var profileImgUrl = 'http://coolpicks.com/johnjacob.jpg';
+
+// var loginWelcomeMessage = "Welcome, " + firstName + ".Happy " + age + "rdbirthday!";
+
+// console.log(loginWelcomeMessage);
+
+var age = 23;
