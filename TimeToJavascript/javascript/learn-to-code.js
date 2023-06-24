7fd7e0ecbbcafacf295e0537513f0a16d18184bc
DcViet <tranducviet.eng@gmail.com>
2023-06-24 12:01:47 +0700

    update

--- TimeToJavascript/javascript/learn-to-code.js
+++ TimeToJavascript/javascript/learn-to-code.js
@@ -13,4 +13,13 @@
 
 // console.log(loginWelcomeMessage);
 
-var age = 23;
+var sum = 10 + 15;
+var sub = 15 - 10;
+var mul = 10 * 3;
+var div = 10 / 3;
+var mod = 10 % 3;
+
+var msg = "10/3 = 3 with a remainder of " + mod;
+var result =10*((5+3)-4);
+
+console.log(result);
