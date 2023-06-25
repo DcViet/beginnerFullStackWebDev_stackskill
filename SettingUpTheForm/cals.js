f5bc32f67d800b4e99975adea1699f676b8be55c
DcViet <tranducviet.eng@gmail.com>
2023-06-25 15:20:20 +0700

    starterTemplate

--- SettingUpTheForm/cals.js
+++ SettingUpTheForm/cals.js
@@ -1,6 +1,9 @@
 var numField1 = document.getElementById('numField1');
 var numField2 = document.getElementById('numField2');
 var resulfField = document.getElementById('resultField');
+// document.getElementById('xIsWhatPercentOfY').addEventListener('submit',function() {
+
+// })
 var form = document.getElementById('xIsWhatPercentOfY');
 
 // var myCalFuntion = funtion() {
@@ -11,13 +14,44 @@ var form = document.getElementById('xIsWhatPercentOfY');
 //     alert("We clickerclacked something!");
 // });
 
-form.addEventListener('submit',function() {
+// form.addEventListener('submit', function() {
+
+//     if (!numField1.value || !numField2.value) {
+//         alert("Please enter value in the fields");
+//     } else {
+//         // var x = numField1.value;
+//         // var y = numField2.value;
+
+//         var x = parseFloat(numField1.value);
+//         var y = parseFloat(numField2.value);
+
+//         // console.log(x + y);
+
+//         alert(x + y);
+//     }
+
+// });
+
+//Percentage Calculator - Algorithm and Prevent Default
+form.addEventListener('submit', function() {
 
     if (!numField1.value || !numField2.value) {
         alert("Please enter value in the fields");
     } else {
-    var x = numField1.value;
-    var y = numField2.value;
+        // var x = numField1.value;
+        // var y = numField2.value;
+
+        var x = parseFloat(numField1.value);
+        var y = parseFloat(numField2.value);
+
+        // console.log(x + y);
+
+        var result = x / y;
+        var percent = result * 100;
+
+        resulfField.innerText = "Answer:" + percent + "%";
+        envent.precentDefault();
+
     }
 
-})
+});
